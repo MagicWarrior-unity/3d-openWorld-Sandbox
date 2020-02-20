1dcc2b352f9c592262bc0b65f79bc099240978d0
MagicWarrior-unity <MagicWarrior-unity@gmail.com>
2020-02-19 21:18:46 -0700

    Added basic perlin noice ground

---
+++ 3d-openWorld/Assets/MeshGenerator.cs
@@ -0,0 +1,94 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(MeshFilter))]
+public class MeshGenerator : MonoBehaviour {
+
+    Mesh mesh;
+
+    Vector3[] vertices;
+    int[] triangles;
+
+    public int xSize = 16;
+    public int zSize = 16;
+    public int ySize = 16;
+    public float scale = 20f;
+    public float max = 0.7f;
+    public float min = 0.2f;
+    void Start()
+    {
+        mesh = new Mesh();
+        GetComponent<MeshFilter>().mesh = mesh;
+
+        CreateSurface();
+        CreateUnderGround();
+        UpdateMesh();
+    }
+
+    void CreateSurface()
+    {
+        vertices = new Vector3[(xSize + 1) * (zSize + 1)];
+
+        for (int z = 0, i = 0; z <= zSize; z++)
+        {
+            for (int x = 0; x <= xSize; x++)
+            {
+                float add = Random.Range(1, 10);
+                float y = Mathf.PerlinNoise(x * 0.1f + add, z * 0.1f + add) * 2f - 0.2f;
+                if (y < min)
+                    y = min;
+                if (y > max)
+                    y = max;
+                vertices[i] = new Vector3(x, y, z);
+                i++;
+            }
+        }
+        int vert = 0;
+        int tris = 0;
+        triangles = new int[xSize * zSize * 6];
+        for (int z = 0; z < zSize; z++)
+        {
+            for (int x = 0; x < zSize; x++)
+            {
+
+                triangles[tris + 0] = vert + 0;
+                triangles[tris + 1] = vert + xSize + 1;
+                triangles[tris + 2] = vert + 1;
+                triangles[tris + 3] = vert + 1;
+                triangles[tris + 4] = vert + xSize + 1;
+                triangles[tris + 5] = vert + xSize + 2;
+
+                vert++;
+                tris += 6;
+            }
+            vert++;
+
+        }
+        
+
+       
+    }
+    void CreateUnderGround()
+    {
+
+    }
+    void UpdateMesh()
+    {
+        mesh.Clear();
+
+        mesh.vertices = vertices;
+        mesh.triangles = triangles;
+        mesh.RecalculateNormals();
+    }
+
+    private void OnDrawGizmos()
+    {
+        if (vertices == null)
+            return;
+        for (int i = 0; i < vertices.Length; i++)
+        {
+            Gizmos.DrawSphere(vertices[i], .1f);
+        }
+    }
+}
